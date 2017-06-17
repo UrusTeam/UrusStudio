9059305be87d39eedfd9ac4dec6393d0b909129a
hiro2233 <htakey@gmail.com>
2017-06-17 17:33:03 -0400

    debian: udpated scripts installers for urus studio.

--- build-ci.sh
+++ build-ci.sh
@@ -11,18 +11,18 @@ export WXURUSTOPDIR=$(pwd)
 mkdir -p buildwx
 cd buildwx
 
-../configure --with-gtk --enable-static=yes --prefix=${WXURUSTOPDIR}/install --enable-unicode --with-flavour=urus --enable-vendor=urus --enable-monolithic
+../configure --with-gtk --enable-static=yes --prefix=${URUSSTDTOPDIR}/install --enable-unicode --with-flavour=urus --enable-vendor=urus --enable-monolithic
 make -j2
 make install
 
-export PATH=${WXURUSTOPDIR}/install/bin:$PATH
+export PATH=${URUSSTDTOPDIR}/install/bin:$PATH
 export ACLOCAL_FLAGS="-I `wx-config --prefix`/share/aclocal"
 
 cd ${URUSSTDTOPDIR}
 ./bootstrap
 mkdir -p buildustd
 cd buildustd
-../configure --with-contrib-plugins="AutoVersioning, BrowseTracker, Cccc, CppCheck, cbkoders, codesnippets,codestat, copystrings, Cscope, dragscroll, EditorConfig, EditorTweaks, envvars,FileManager, headerfixup, hexeditor, incsearch, keybinder, libfinder, MouseSap, ProjectOptionsManipulator, profiler, regex, ReopenEditor,smartindent,symtab, ThreadSearch, wxcontrib, wxsmith, wxsmithcontrib, wxsmithaui" --prefix=${WXURUSTOPDIR}/install --with-platform=gtk
+../configure --with-contrib-plugins="AutoVersioning, BrowseTracker, Cccc, CppCheck, cbkoders, codesnippets,codestat, copystrings, Cscope, dragscroll, EditorConfig, EditorTweaks, envvars,FileManager, headerfixup, hexeditor, incsearch, keybinder, libfinder, MouseSap, ProjectOptionsManipulator, profiler, regex, ReopenEditor,smartindent,symtab, ThreadSearch, wxcontrib, wxsmith, wxsmithcontrib, wxsmithaui" --prefix=${URUSSTDTOPDIR}/install --with-platform=gtk
 make -j2
 make install
 
