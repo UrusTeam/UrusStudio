347261c881807e673aefd3b964f3e16ee4321323
hiro2233 <htakey@gmail.com>
2017-06-23 12:34:46 -0400

    ci: updated ci script build.

--- build-ci.sh
+++ build-ci.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-export URUSSTDTOPDIR=$(pwd)
+# MSYSTEM is defined when cross-compilig with MinGW/MSYS
+if [ "x$MSYSTEM" != "x" ] ; then
+  echo Updating MinGW32/cross version.
+  WXURUSBUILD="--build=i686-w64-mingw32"
+  WXURUSHOST="--host=i686-w64-mingw32"
+  WXURUSTARGET="--target=i686-w64-mingw32"
+  WXURUSCONF="--with-msw --enable-monolithic --with-libiconv"
+  URUSSTUDIOPLAT="--with-platform=win32"
+else
+  uname_str=$(uname)
+  if [ "$uname_str" = "Darwin" ] ; then
+    echo Updating Mac / Darwin version.
+    WXURUSBUILD=""
+    WXURUSHOST=""
+    WXURUSTARGET=""
+    WXURUSCONF="--with-mac --enable-monolithic"
+    URUSSTUDIOPLAT="--with-platform=macosx"
+  else
+    echo Updating Linux version.
+    WXURUSBUILD=""
+    WXURUSHOST=""
+    WXURUSTARGET=""
+    WXURUSCONF="--with-gtk --enable-monolithic"
+    URUSSTUDIOPLAT="--with-platform=gtk"
+  fi
+fi
 
-#git submodule init
-#git submodule update
+export URUSSTDTOPDIR=$(pwd)
 
 cd modules/wxWidgets
 
@@ -11,20 +35,24 @@ export WXURUSTOPDIR=$(pwd)
 mkdir -p buildwx
 cd buildwx
 
-../configure --with-gtk --prefix=${URUSSTDTOPDIR}/install --enable-unicode --with-flavour=urus --enable-vendor=urus --enable-monolithic
-make -j2
+../configure $WXURUSBUILD $WXURUSHOST $WXURUSTARGET --prefix=${URUSSTDTOPDIR}/install --enable-unicode --with-flavour=urus --enable-vendor=urus $WXURUSCONF
+make
 make install
 
-export PATH=${URUSSTDTOPDIR}/install/bin:$PATH
-export ACLOCAL_FLAGS="-I `wx-config --prefix`/share/aclocal"
+#Only for Unix Like, on Windows we don't make it.
+if [ "x$MSYSTEM" == "x" ] ; then
+    export PATH=${URUSSTDTOPDIR}/install/bin:$PATH
+    export ACLOCAL_FLAGS="-I `wx-config --prefix`/share/aclocal"
 
-cd ${URUSSTDTOPDIR}
-./bootstrap
-mkdir -p buildustd
-cd buildustd
-../configure --with-contrib-plugins="AutoVersioning, BrowseTracker, Cccc, CppCheck, cbkoders, codesnippets,codestat, copystrings, Cscope, dragscroll, EditorConfig, EditorTweaks, envvars,FileManager, headerfixup, hexeditor, incsearch, keybinder, libfinder, MouseSap, ProjectOptionsManipulator, profiler, regex, ReopenEditor,smartindent,symtab, ThreadSearch, ToolsPlus, Valgrind, wxcontrib, wxsmith, wxsmithcontrib, wxsmithaui" --prefix=${URUSSTDTOPDIR}/install --with-platform=gtk
-make -j2
-make install
+    cd ${URUSSTDTOPDIR}
+    ./bootstrap
+    mkdir -p buildustd
+    cd buildustd
+
+    ../configure $WXURUSBUILD $WXURUSHOST $WXURUSTARGET --with-contrib-plugins="AutoVersioning, BrowseTracker, Cccc, CppCheck, cbkoders, codesnippets,codestat, copystrings, Cscope, dragscroll, EditorConfig, EditorTweaks, envvars,FileManager, headerfixup, hexeditor, incsearch, keybinder, libfinder, MouseSap, ProjectOptionsManipulator, profiler, regex, ReopenEditor,smartindent,symtab, ThreadSearch, wxcontrib, wxsmith, wxsmithcontrib, wxsmithaui" --prefix=${URUSSTDTOPDIR}/install $URUSSTUDIOPLAT
+    make
+    make install
+fi
 
 echo BUILD OK!
 exit 0
