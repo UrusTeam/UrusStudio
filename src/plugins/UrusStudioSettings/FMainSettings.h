5709d6a0ec9a737e3ef92408099d4380baea6bdf
hiro2233 <htakey@gmail.com>
2018-02-04 11:32:41 -0400

    plugins: added override globals vars checkbox and func.

--- src/plugins/UrusStudioSettings/FMainSettings.h
+++ src/plugins/UrusStudioSettings/FMainSettings.h
@@ -3,6 +3,7 @@
 
 //(*Headers(FMainSettings)
 #include <wx/button.h>
+#include <wx/checkbox.h>
 #include <wx/panel.h>
 #include <wx/sizer.h>
 //*)
@@ -15,15 +16,22 @@ class FMainSettings: public wxPanel
 		virtual ~FMainSettings();
 
 		//(*Declarations(FMainSettings)
+		wxBoxSizer* BoxSizer1;
+		wxBoxSizer* BoxSizer2;
+		wxBoxSizer* BoxSizer3;
 		wxButton* Button1;
 		wxButton* Button2;
+		wxButton* Button3;
+		wxCheckBox* CheckBox1;
 		//*)
 
 	protected:
 
 		//(*Identifiers(FMainSettings)
+		static const long ID_CHECKBOX1;
 		static const long ID_BUTTON1;
 		static const long ID_BUTTON2;
+		static const long ID_BUTTON3;
 		//*)
 
 	private:
@@ -31,6 +39,7 @@ class FMainSettings: public wxPanel
 		//(*Handlers(FMainSettings)
 		void OnButton1Click(wxCommandEvent& event);
 		void OnButton2Click(wxCommandEvent& event);
+		void OnButton3Click(wxCommandEvent& event);
 		//*)
 
 		DECLARE_EVENT_TABLE()
