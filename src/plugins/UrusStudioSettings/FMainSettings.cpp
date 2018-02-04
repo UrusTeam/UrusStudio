21b98ff3500b13757748826e1016abf13017a6b3
hiro2233 <htakey@gmail.com>
2018-02-04 11:32:41 -0400

    plugins: added override globals vars checkbox and func.

--- src/plugins/UrusStudioSettings/FMainSettings.cpp
+++ src/plugins/UrusStudioSettings/FMainSettings.cpp
@@ -1,5 +1,6 @@
 #include "FMainSettings.h"
 #include <wx/msgdlg.h>
+#include <configmanager.h>
 #include <manager.h>
 #include <wx/wx.h>
 #include "urusstudiosettings.h"
@@ -12,8 +13,10 @@ urusstudiosettings* main_urusstudio = URUS_STUDIO_SETTINGS::GetUrusStudioMain();
 //*)
 
 //(*IdInit(FMainSettings)
+const long FMainSettings::ID_CHECKBOX1 = wxNewId();
 const long FMainSettings::ID_BUTTON1 = wxNewId();
 const long FMainSettings::ID_BUTTON2 = wxNewId();
+const long FMainSettings::ID_BUTTON3 = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(FMainSettings,wxPanel)
@@ -24,20 +27,28 @@ END_EVENT_TABLE()
 FMainSettings::FMainSettings(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size)
 {
 	//(*Initialize(FMainSettings)
-	wxBoxSizer* BoxSizer1;
-
-	Create(parent, id, wxDefaultPosition, wxSize(315,202), wxTAB_TRAVERSAL, _T("id"));
+	Create(parent, id, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("id"));
 	BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
-	Button1 = new wxButton(this, ID_BUTTON1, _("Close"), wxDefaultPosition, wxSize(88,23), 0, wxDefaultValidator, _T("ID_BUTTON1"));
-	BoxSizer1->Add(Button1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer3 = new wxBoxSizer(wxVERTICAL);
+	CheckBox1 = new wxCheckBox(this, ID_CHECKBOX1, _("Override Global Vars."), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_CHECKBOX1"));
+	CheckBox1->SetValue(false);
+	BoxSizer3->Add(CheckBox1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer1->Add(BoxSizer3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer2 = new wxBoxSizer(wxVERTICAL);
+	Button1 = new wxButton(this, ID_BUTTON1, _("Close"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
+	BoxSizer2->Add(Button1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	Button2 = new wxButton(this, ID_BUTTON2, _("Test!"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-	BoxSizer1->Add(Button2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer2->Add(Button2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	Button3 = new wxButton(this, ID_BUTTON3, _("Save"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
+	BoxSizer2->Add(Button3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer1->Add(BoxSizer2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	SetSizer(BoxSizer1);
-	SetSizer(BoxSizer1);
-	Layout();
+	BoxSizer1->Fit(this);
+	BoxSizer1->SetSizeHints(this);
 
 	Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FMainSettings::OnButton1Click);
 	Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FMainSettings::OnButton2Click);
+	Connect(ID_BUTTON3,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FMainSettings::OnButton3Click);
 	//*)
 }
 
@@ -57,4 +68,16 @@ void FMainSettings::OnButton1Click(wxCommandEvent& event)
 void FMainSettings::OnButton2Click(wxCommandEvent& event)
 {
     main_urusstudio->TestCommand();
+}
+
+void FMainSettings::OnButton3Click(wxCommandEvent& event)
+{
+    if (CheckBox1->IsChecked()) {
+        Manager::Get()->GetConfigManager(_T("app"))->Write(_T("/ur_overrsettings"), true);
+    } else {
+        Manager::Get()->GetConfigManager(_T("app"))->Write(_T("/ur_overrsettings"), false);
+    }
+
+    wxMessageBox(wxString::Format(_("%s"), _("UrusStudio settings saved!\n")) +
+                 wxString::Format(_("%s"), _("Restart Urus Studio to take effect the changes!")));
 }
