caea9e5e31f262a860c875debe1a910d7ff9be08
hiro2233 <htakey@gmail.com>
2018-02-12 19:45:52 -0400

    src: Updated welcome msg.

--- src/src/dlgabout.cpp
+++ src/src/dlgabout.cpp
@@ -58,10 +58,12 @@ dlgAbout::dlgAbout(wxWindow* parent)
                                  _(" is a full-featured IDE (Integrated Development Environment) "
                                    "aiming to make the individual developer (and the development team) "
                                    "work in a nice programming environment offering everything he/they "
-                                   "would ever need from a program of that kind.\n"
-                                   "Its pluggable architecture allows you, the developer, to add "
-                                   "any kind of functionality to the core program, through the use of "
-                                   "plugins...\n");
+                                   "would ever need from a program of that kind, focused and oriented "
+                                   "for embbed development based on URUS and Ardupilot autopilot software.\n\n"
+                                   "Urus Studio is based on Code::Blocks and Its pluggable architecture allows "
+                                   "you, the developer, to add any kind of functionality to the core program, "
+                                   "through the use of plugins for enchance the capabilites of the Urus Studio IDE "
+                                   "for your purposes.\n");
 
     wxString file = ConfigManager::ReadDataPath() + _T("/images/urus_splash.png");
 
