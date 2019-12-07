4355595c0e15605d4ea06352734f72c5c90b06d2
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2019-12-07 06:27:20 -0400

    build: fixed URUSINSTALLDIR envar init.

--- build-ci.sh
+++ build-ci.sh
@@ -43,16 +43,14 @@ export WXURUSTOPDIR=$(pwd)
 
 cd $URUSINSTALLDIR
 
-if [ "x$ENABLEGIT" != "x" ] ; then
-  if [ -e $URUSINSTALLDIR/.git ] ; then
-    echo "git ok"
-  else
-    echo "git init"
-    if [ "x$ENABLEGIT" != "x" ] ; then
-      git init $URUSINSTALLDIR
-      git config --global user.email $(printf "%s@%s" ${USER} $(uname -n))
-      git config --global user.name ${USER}
-    fi
+if [ -e $URUSINSTALLDIR/.git ] ; then
+  echo "git ok"
+else
+  echo "git init"
+  if [ "x$ENABLEGIT" != "x" ] ; then
+    git init $URUSINSTALLDIR
+    git config --global user.email $(printf "%s@%s" ${USER} $(uname -n))
+    git config --global user.name ${USER}
   fi
 fi
 
