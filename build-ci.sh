edcf89689e4b01c35f3fee7ef78daa0b61e94d92
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-07-20 08:15:13 -0400

    build: added space instead tab on NO_URUSSTUDIO_MAKE_CMD.

--- build-ci.sh
+++ build-ci.sh
@@ -118,10 +118,10 @@ if [ "x$NO_BUILD_ALL" = "x" ] ; then
         cd $PUSHD
         ../configure $WXURUSBUILD $WXURUSHOST $WXURUSTARGET --with-contrib-plugins="BrowseTracker,Cscope,EditorConfig,EditorTweaks,envvars,hexeditor,incsearch,ProjectOptionsManipulator,regex,ReopenEditor,smartindent,ThreadSearch,wxcontrib,wxsmith,wxsmithcontrib,wxsmithaui" --prefix=${URUSINSTALLDIR} $URUSSTUDIOPLAT
 
-		if [ "x$NO_URUSSTUDIO_MAKE_CMD" = "x"  ] ; then
-        	make -j2
-        	make install
-		fi
+        if [ "x$NO_URUSSTUDIO_MAKE_CMD" = "x" ] ; then
+            make -j2
+            make install
+        fi
     fi
 fi
 
